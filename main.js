3b51a13adc07ce7b95b8537b9e22ce892256f794
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-28 22:23:04 +0500

    lesson15

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// const API_KEY = 'https://fakestoreapi.com/products/';
+
+// fetch('https://fakestoreapi.com/products/', {method: 'get'})
+//     .then(a => a.json())
+//     .then(b => console.log(b))
+//     .catch(c => console.error(c))
+
+// axios('https://fakestoreapi.com/products/')
+//     .then(a => console.log(a.data))
+//     .catch(b => console.log(b));
